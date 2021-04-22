1bb8453fec5b9db351fc1acacbdff9940f3d6bf6
DANIEL BISPO MONTEIRO <60003488@midway.com.br>
2021-04-22 00:05:19 -0300

    Forgot a shit ton of commits on the way, kudos to me. but here's the go part ready :)

--- go/main.go
+++ go/main.go
@@ -1,5 +1,23 @@
-package hello
+package main
 
-func Hello() string {
-	return "Hello, World."
+import (
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/bispoman/gotalk/router"
+)
+
+func main() {
+
+	router := router.GetRoutes()
+
+	http.Handle("/", router)
+
+	port := ":8080"
+
+	log.Info("Application has been started at port: ", port)
+
+	log.Fatal(http.ListenAndServe(port, nil))
+
 }
